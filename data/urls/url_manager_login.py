7efdea7a4d196bc97f2a975995f2093bc5fceda2
yerkebulan_a <73381587+eabdikhalyk@users.noreply.github.com>
2026-07-01 08:23:49 +0500

    Update BASE_URL for login manager

--- data/urls/url_manager_login.py
+++ data/urls/url_manager_login.py
@@ -1,3 +1,3 @@
-BASE_URL = "https://alpha.business-t.berekebank.kz"
+BASE_URL = "https://alpha.business.kz"
 LOGIN = f"{BASE_URL}/manager/login?next=/management/managers"
 HYUNDAI_AUTH_PERSON = f"{BASE_URL}/manager/companyPersons/455"
